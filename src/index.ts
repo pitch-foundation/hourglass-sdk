1d3ef7236497d75082040929f280bc0e078ec88f
Alex Roman <alex@hourglass.com>
2024-03-27 12:14:35 +0200

    export constants

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from './chain/chain.adaptors';
 export * from './events/events.rfq-provider';
 export * from './events/events.data-provider';
 export * from './events/events.types';
+export * from './events/events.constants';
 
 export * from './seaport/seaport.utils';
 export * from './seaport/seaport.types';
