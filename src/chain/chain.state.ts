b4686ab7907bfb3aa6fcc4a6d57a30bcf8567209
TBIQ <9085296+bam4564@users.noreply.github.com>
2024-04-11 14:37:28 -0700

    werk-final-final

--- src/chain/chain.state.ts
+++ src/chain/chain.state.ts
@@ -2,17 +2,17 @@ import { JsonRpcSigner } from '@ethersproject/providers';
 import { ERC20__factory } from '../abi';
 import { STAKING_ADDRESSES } from '../seaport/seaport.utils';
 
-export const getBalance = async (
+export const getBalanceAndAllowance = async (
   signer: JsonRpcSigner,
-  tokenAddress: string
+  tokenAddress: string,
+  spender = STAKING_ADDRESSES.seaportConduit
 ) => {
   const signerAddress = await signer.getAddress();
-  const assetInstance = ERC20__factory.connect(tokenAddress, signer);
-  const assetBalance = await assetInstance.balanceOf(signerAddress);
-  const assetAllowance = await assetInstance.allowance(
-    signerAddress,
-    STAKING_ADDRESSES.seaportConduit
-  );
+  const erc20 = ERC20__factory.connect(tokenAddress, signer);
+  const [assetBalance, assetAllowance] = await Promise.all([
+    erc20.balanceOf(signerAddress),
+    erc20.allowance(signerAddress, spender),
+  ]);
   return {
     assetBalance,
     assetAllowance,
