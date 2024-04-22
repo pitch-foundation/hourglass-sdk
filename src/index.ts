3684397230fa4f2cdfb38a06e39a161a0a4ef57d
TBIQ <9085296+bam4564@users.noreply.github.com>
2024-04-21 19:32:25 -0700

    file renaming / moving

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ export * from './chain/chain.state.js';
 export * from './chain/chain.actions.js';
 export * from './chain/chain.adaptors.js';
 
-export * from './events/events.taker-provider.js';
-export * from './events/events.data-provider.js';
-export * from './events/events.maker-provider.js';
-export * from './events/providers.types.js';
-export * from './events/events.constants.js';
+export * from './providers/public/taker.provider.js';
+export * from './providers/public/data.provider.js';
+export * from './providers/public/maker.provider.js';
+export * from './providers/providers.types.js';
+export * from './providers/providers.constants.js';
 
 export * from './seaport/seaport.utils.js';
 export * from './seaport/seaport.types.js';
